ebc8d6fb5526e1eeae42b6c833c2a3ebe74740ad
tee_zed <tee0zed@gmail.com>
2020-05-25 13:31:12 -0400

    forms fixed, ui improvements, events validations added

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,16 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+  validates :title,
+            presence: true,
+            length: { maximum: 75 }
+
+  validates :description,
+            presence: true,
+            length: { maximum: 355 }
+
+  validates :address,
+            presence: true,
+            length: { maximum: 150 }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   resources :events
   resources :users, only: [:show, :edit, :update]
+
+
 end
