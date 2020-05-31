17857910545c4a62f6c77d10832ca251a0b82261
tee0zed <tee@zed@gmail.com>
2020-05-31 16:19:38 +0300

    email uniq validation for users & prohibition of event creators become subs of own events added

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,6 @@
 module EventsHelper
-  def render_events
+  def render_events(collection)
     return "Пока нет ни одного события." unless @events.present?
-    render partial: 'partials/event', collection: @events
+    render partial: 'partials/event', collection: collection
   end
 end
