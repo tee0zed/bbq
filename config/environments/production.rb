1e65d9ebffdd9e992329d3d751a08b32aa98c75a
tee0zed <tee@zed@gmail.com>
2020-07-15 21:22:19 +0300

    deploy bundle

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,8 @@ Rails.application.configure do
     address:         'smtp.sendgrid.net',
     port:            '587',
     authentication:  :plain,
-    user_name:        ENV['SENDGRID_USERNAME'],
-    password:         ENV['SENDGRID_PASSWORD'],
+    user_name:        Rails.application.credentials.dig(:sendgrid, :SENDGRID_USERNAME),
+    password:         Rails.application.credentials.dig(:sendgrid, :SENDGRID_PASSWORD),
     domain:          'heroku.com',
     enable_starttls_auto:  true
   }
